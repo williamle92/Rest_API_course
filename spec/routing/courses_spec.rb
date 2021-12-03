3c10c5eb4d2cd6e6aed922e0d9ffe0ec4bf8618c
williamle92 <william1992le@gmail.com>
2021-12-03 14:53:36 -0800

    created courses controller and routes for courses

--- spec/routing/courses_spec.rb
+++ spec/routing/courses_spec.rb
@@ -1,8 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe '/courses routes' do
-    it 'routes to courses#index' do 
-        expect(get '/courses').to route_to('courses#index')
-        expect(get '/courses?page[number]=3').to route_to('courses#index', page: {'number':3})
+    it 'routes to courses#index' do
+        aggregate_failures do
+            expect(get '/courses').to route_to('courses#index')
+            expect(get '/courses?page[number]=3').to route_to('courses#index', page: {'number':"3"})
+        end    
     end
 end
